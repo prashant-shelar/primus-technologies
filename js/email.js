517523e07ce44713fa4e451b4221a9c4f3a18c23
Prashant Shelar <prashant_shelar@yahoo.com>
2025-08-15 21:24:20 +0530

    Added EmailJs configuration

--- js/email.js
+++ js/email.js
@@ -1,11 +1,18 @@
-function sendEmail(){
-    let params = {
-        name: document.getElementById("firstname").value + document.getElementById("lastname").value,
-        email: document.getElementById("email").value,
-        gender: document.getElementById("occupation").value,
-        phone: "+91"+ document.getElementById("phone").value,
-        address: document.getElementById("address").value,
-        attachment: document.getElementById("upload").value
+function sendEmail() {
+  let params = {
+    name:document.getElementById("firstname").value + document.getElementById("lastname").value,
+    email: document.getElementById("email").value,
+    gender: document.getElementById("occupation").value,
+    phone: "+91" + document.getElementById("phone").value,
+    address: document.getElementById("address").value,
+    attachment: document.getElementById("upload").value,
+  };
+  emailjs.send("service_679qief", "template_lalpomj", params).then(
+    () => {
+      alert("Email Sent Successfully!!!");
+    },
+    (error) => {
+      alert("Failed....", error);
     }
-   emailjs.send("Service_ID","template_ID",params).then(alert("Email Sent Successfully!!!"));l
+  );
 }
